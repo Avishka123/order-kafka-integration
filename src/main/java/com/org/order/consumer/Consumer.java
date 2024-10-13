845d3a6f5885fa22496eef332a551cd82f299a74
Avishka Rathnayake <fsy7uawb4o4g1ibney1o3pkgl92biv@bots.bitbucket.org>
2024-10-13 21:53:51 +0530

    feat - consumer batch processing

--- src/main/java/com/org/order/consumer/Consumer.java
+++ src/main/java/com/org/order/consumer/Consumer.java
@@ -6,39 +6,77 @@ import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.listener.ConsumerSeekAware;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
 
 @Component
 @Slf4j
 public class Consumer implements ConsumerSeekAware {
 
-  @Value("${seek.offset.enable}")
-  private boolean offsetEnable;
+    @Value("${seek.offset.enable}")
+    private boolean offsetEnable;
 
-  @Value("${seek.offset}")
-  private long offset;
+    @Value("${seek.offset}")
+    private long offset;
 
-  @KafkaListener(groupId = "${group-id}", topics = "${topic.name}")
-  public void consume(ConsumerRecord<String, String> record) {
-    log.info(
-        "Consumed the order for (TP: {}, P: {}, OFF_SET: {}, K: {}, V: {})",
-        record.topic(),
-        record.partition(),
-        record.offset(),
-        record.key(),
-        record.value());
-  }
-
-  @Override
-  public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
-
-    if (offsetEnable){
-      assignments.forEach((tp,off)->{
-        callback.seek(tp.topic(),tp.partition(),offset);
-      });
+    @KafkaListener(groupId = "${group-id}", topics = "${topic.name}", autoStartup = "false")
+    public void consume(ConsumerRecord<String, String> record) {
+        log.info(
+                "Consumed the order for (TP: {}, P: {}, OFF_SET: {}, K: {}, V: {})",
+                record.topic(),
+                record.partition(),
+                record.offset(),
+                record.key(),
+                record.value());
     }
 
-  }
+    @KafkaListener(groupId = "${group-id}", topics = "${topic.name}", batch = "true")
+    public void consume(List<ConsumerRecord<String, String>> records, Acknowledgment acknowledgment) {
+
+        try {
+
+            records.forEach(record -> {
+                try {
+                    log.info(
+                            "Consumed the order for (TP: {}, P: {}, OFF_SET: {}, K: {}, V: {})",
+                            record.topic(),
+                            record.partition(),
+                            record.offset(),
+                            record.key(),
+                            record.value());
+                } catch (Exception e) {
+                    log.error(
+                            "Error processing order for (TP: {}, P: {}, OFF_SET: {}, K: {}, V: {}, ERR: {})",
+                            record.topic(),
+                            record.partition(),
+                            record.offset(),
+                            record.key(),
+                            record.value(), e.getMessage());
+                }
+            });
+
+            log.info(
+                    "Consumed the batch of order for (BATCH_SIZE: {})",
+                    records.size());
+
+        } catch (Exception e) {
+            log.error(
+                    "Error processing batch (ERR: {})",
+                     e.getMessage());
+        }finally {
+            acknowledgment.acknowledge();
+        }
+
+
+    }
+
+    @Override
+    public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback cb) {
+        if (offsetEnable) {
+            assignments.forEach((tp, off) -> cb.seek(tp.topic(), tp.partition(), offset));
+        }
+    }
 }
